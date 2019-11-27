0169a71c129ffc0f677c5ae454eed247c150d8ba
WestSize <gfdstyle@gmail.com>
2019-11-27 18:10:23 +0200

    third commit

---
+++ src/main/java/com/example/hmmsbeta1/web/services/UserPrincipal.java
@@ -0,0 +1,63 @@
+package com.example.hmmsbeta1.web.services;
+
+import com.example.hmmsbeta1.web.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        this.user.getRoles().forEach(p->{GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_"+p);
+        authorities.add(authority);
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFirstName() {
+        return this.user.getFirstName();
+    }
+
+    public String getLastName() { return this.user.getLastName();}
+}
